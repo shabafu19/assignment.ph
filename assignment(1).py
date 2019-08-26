995efa0032509deb601f8373c485429fe2163789
shabafu19 <54536194+shabafu19@users.noreply.github.com>
2019-08-26 12:46:39 +0100

    Add files via upload

---
+++ assignment(1).py
@@ -0,0 +1,14 @@
+import math
+
+x = int(input("Enter X Number:"))
+
+y = int(input("Enter Y Number:"))
+
+answer = x**y
+
+print (answer)
+
+print ("x raised to the power of y is:", answer)
+
+print (math.log(x, 2))
+print (math.log2(x))
